a32a1de39fae7b5f204156c5d0f570547720fdc2
Gia Ly Nguyen <gialynguyen@Gias-MacBook-Air.local>
2023-01-25 22:04:04 +0700

    feat: upgrade nvim-dashboard new version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,7 +47,75 @@ packer.startup(function()
 
   use "kyazdani42/nvim-web-devicons"
 
-  use "glepnir/dashboard-nvim"
+  use {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup {
+        theme = "doom",
+        config = {
+          header = {
+
+            "",
+            "",
+            "",
+            "",
+            " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+            " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+            " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+            " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+            " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+            " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+            "",
+            "",
+            "",
+          },
+          center = {
+            {
+              icon = " ",
+              desc = "Recently latest session      ",
+              action = "SessionManager load_session",
+            },
+            {
+              icon = " ",
+              desc = "Recently opened files    ",
+              action = "Telescope oldfiles",
+              shortcut = ", re",
+            },
+            {
+              icon = " ",
+              desc = "New File                  ",
+              action = "DashboardNewFile",
+              shortcut = ", o",
+            },
+            {
+              icon = " ",
+              desc = "Browse Files              ",
+              action = "Telescope file_browser",
+              shortcut = ", n",
+            },
+            {
+              icon = " ",
+              desc = "Find File                 ",
+              action = "Telescope find_files",
+              shortcut = ", f",
+            },
+            {
+              icon = " ",
+              desc = "Configure Neovim          ",
+              action = "edit ~/.config/nvim/init.vim",
+              shortcut = ", v",
+            },
+            {
+              icon = " ",
+              desc = "Exit Neovim                  ",
+              action = "quit",
+            },
+          },
+        },
+      }
+    end,
+  }
 
   use {
     "sainnhe/gruvbox-material",
@@ -884,64 +952,6 @@ require("gitsigns").setup {
   numhl = true,
 }
 
-local db = require "dashboard"
-db.custom_header = {
-  "",
-  "",
-  "",
-  "",
-  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-  "",
-  "",
-  "",
-}
-db.custom_center = {
-  {
-    icon = " ",
-    desc = "Recently latest session      ",
-    action = "SessionManager load_session",
-  },
-  {
-    icon = " ",
-    desc = "Recently opened files    ",
-    action = "Telescope oldfiles",
-    shortcut = ", re",
-  },
-  {
-    icon = " ",
-    desc = "New File                  ",
-    action = "DashboardNewFile",
-    shortcut = ", o",
-  },
-  {
-    icon = " ",
-    desc = "Browse Files              ",
-    action = "Telescope file_browser",
-    shortcut = ", n",
-  },
-  {
-    icon = " ",
-    desc = "Find File                 ",
-    action = "Telescope find_files",
-    shortcut = ", f",
-  },
-  {
-    icon = " ",
-    desc = "Configure Neovim          ",
-    action = "edit ~/.config/nvim/init.vim",
-    shortcut = ", v",
-  },
-  {
-    icon = " ",
-    desc = "Exit Neovim                  ",
-    action = "quit",
-  },
-}
 vim.keymap.set("n", "<Leader>o", ":DashboardNewFile<CR>", { silent = true })
 
 local Path = require "plenary.path"
