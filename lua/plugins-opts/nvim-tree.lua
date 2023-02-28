18cb5ae5e7ba332072f29d277ffb3cc7c4fa866b
Gia Ly Nguyen <gialynguyen@Gias-MacBook-Air.local>
2023-03-01 00:59:24 +0700

    fix: workaround highlight error when open nvim-tree

--- lua/plugins-opts/nvim-tree.lua
+++ lua/plugins-opts/nvim-tree.lua
@@ -22,10 +22,23 @@ require("nvim-tree").setup {
 local api = require "nvim-tree.api"
 local Event = api.events.Event
 
+local need_workaround_update_colorschema = false
+
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+  callback = function()
+    need_workaround_update_colorschema = true
+  end,
+})
+
 api.events.subscribe(Event.TreeOpen, function()
   if vim.g.transparent_enabled == true then
     pcall(vim.cmd, string.format("hi %s ctermbg=NONE guibg=NONE", "NvimTreeNormal"))
     pcall(vim.cmd, string.format("hi %s ctermbg=NONE guibg=NONE", "NvimTreeStatuslineNc"))
     pcall(vim.cmd, string.format("hi %s ctermbg=NONE guibg=NONE", "NvimTreeEndOfBuffer"))
+  end
+
+  if need_workaround_update_colorschema == true then
+    vim.api.nvim_command(string.format("colorscheme %s", vim.g.colors_name))
+    need_workaround_update_colorschema = false
   end
 end)
